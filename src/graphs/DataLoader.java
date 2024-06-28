5d6ad6260938faf4b224844e03e14a2dc8e80d49
Martin Schäler <martin.schaeler@sbg.ac.at>
2024-06-28 11:42:59 +0200

    intital commit

---
+++ src/graphs/DataLoader.java
@@ -0,0 +1,264 @@
+package graphs;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+
+public class DataLoader {
+	public static String data_file = "./data/enron-mysqldump.sql";
+	static Graph get_enron_graph(){
+		final String is_message = "INSERT INTO message VALUES (";
+		final String is_recipient = "INSERT INTO recipientinfo VALUES (";
+		final int message_mid 		= 0;
+		final int message_sender 	= 1;
+		final int recipient_mid		= 1;
+		final int recipient_email 	= 3;
+		
+		/**
+		 * [mid,email]
+		 */
+		ArrayList<String[]> from = new ArrayList<String[]>(100000);
+		HashMap<String, ArrayList<String>> to = new HashMap<String, ArrayList<String>>(100000);
+		
+		File f = new File(data_file);
+		if(f.exists()) {
+			try(BufferedReader br = new BufferedReader(new FileReader(f))){
+				String line;
+				int i = 0;
+				while((line = br.readLine()) != null) {
+					if(line.startsWith(is_message)) {
+						String[] tokens = line.replace(is_message, "").split(",");
+						String[] temp = {tokens[message_mid],tokens[message_sender]};
+						from.add(temp);
+						//System.out.println(tokens[0]+" "+tokens[1]);
+					}else if(line.startsWith(is_recipient)){
+						String[] tokens = line.replace(is_recipient, "").split(",");
+						ArrayList<String> all_recipients = to.get(tokens[recipient_mid]);
+						if(all_recipients==null) {
+							all_recipients = new ArrayList<String>();
+							to.put(tokens[recipient_mid], all_recipients);
+						}
+						all_recipients.add(tokens[recipient_email]);
+						//System.out.println(tokens[1]+" "+tokens[3]);
+					}
+					
+					if(i++%100000==0)	
+						System.out.println(i+" "+line);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			// get all nodes
+			HashSet<String> all_email_adresses = new HashSet<String>();
+			for(String[] message : from) {
+				all_email_adresses.add(message[1]);
+			}
+			for( Entry<String, ArrayList<String>> entry : to.entrySet()) {
+				for(String email : entry.getValue()) {
+					all_email_adresses.add(email);	
+				}
+			}
+			System.out.println("Got |G| = "+all_email_adresses.size());
+			String[] nodes = new String[all_email_adresses.size()];
+			int counter = 0;
+			for(String s : all_email_adresses) {
+				nodes[counter++] = s;
+			}
+			Arrays.sort(nodes);
+			HashMap<String, Integer> node_to_id = new HashMap<String, Integer>(nodes.length);
+			for(int id=0;id<nodes.length;id++) {
+				node_to_id.put(nodes[id], id);
+			}
+			
+			Graph g = new Graph(all_email_adresses.size());
+			for(String[] message : from) {
+				String mid = message[0];
+				final int start_vertex = node_to_id.get(message[1]);
+				ArrayList<String> recipients = to.get(mid);
+				if(recipients!=null) {
+					for(String node : recipients) {
+						int target_vertex = node_to_id.get(node);
+						//System.out.println(start_vertex+" "+target_vertex);
+						g.add_edge(start_vertex, target_vertex);
+					}
+				}else{
+					//System.err.println("recipients==null "+Arrays.toString(message));
+				}
+			}
+			System.out.println(g);
+			return g;
+		}else{
+			System.err.println(f+" does not exist");
+			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * asserts that employees come first and messages second.
+	 * @return
+	 */
+	@Deprecated
+	static Graph get_enron_graph_employee_unique_edges(){
+		final String is_employee = "INSERT INTO employeelist VALUES (";
+		final String is_message = "INSERT INTO message VALUES (";
+		final String is_recipient = "INSERT INTO recipientinfo VALUES (";
+		final int message_mid 		= 0;
+		final int message_sender 	= 1;
+		final int recipient_mid		= 1;
+		final int recipient_email 	= 3;
+		final int employee_email_id = 3;
+		
+		/**
+		 * [mid,email]
+		 */
+		ArrayList<String[]> from = new ArrayList<String[]>(100000);
+		HashMap<String, ArrayList<String>> to = new HashMap<String, ArrayList<String>>(100000);
+		
+		File f = new File(data_file);
+		if(f.exists()) {
+			try(BufferedReader br = new BufferedReader(new FileReader(f))){
+				String line;
+				int i = 0;
+				/**
+				 * Only of employees
+				 */
+				HashSet<String> email_ids = new HashSet<String>();
+				HashSet<String> mids = new HashSet<String>();
+				
+				while((line = br.readLine()) != null) {
+					if(line.startsWith(is_message)) {
+						String[] tokens = line.replace(is_message, "").split(",");
+						if(email_ids.contains(tokens[message_sender])) {//only messages from enron employees
+							String[] temp = {tokens[message_mid],tokens[message_sender]};
+							from.add(temp);	
+							mids.add(tokens[message_mid]);
+						}
+						//System.out.println(tokens[0]+" "+tokens[1]);
+					}else if(line.startsWith(is_recipient)){
+						String[] tokens = line.replace(is_recipient, "").split(",");
+						if(mids.contains(tokens[recipient_mid])) {//Mail from an enron employee
+							if(email_ids.contains(tokens[recipient_mid])) {//Mail to an enron employee
+								ArrayList<String> all_recipients = to.get(tokens[recipient_mid]);
+								if(all_recipients==null) {
+									all_recipients = new ArrayList<String>();
+									to.put(tokens[recipient_mid], all_recipients);
+								}
+								all_recipients.add(tokens[recipient_email]);
+								//System.out.println(tokens[1]+" "+tokens[3]);
+							}
+						}
+					}else if(line.startsWith(is_employee)){
+						String[] tokens = line.replace(is_employee, "").split(",");
+						String email_id = tokens[employee_email_id].replace(");", "");
+						//System.out.println(email_id);
+						email_ids.add(email_id);
+					}
+					
+					if(i++%100000==0)	
+						System.out.println(i+" "+line);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			// get all nodes
+			HashSet<String> all_email_adresses = new HashSet<String>();
+			for(String[] message : from) {
+				all_email_adresses.add(message[message_sender]);
+			}
+			for( Entry<String, ArrayList<String>> entry : to.entrySet()) {
+				for(String email : entry.getValue()) {
+					all_email_adresses.add(email);	
+				}
+			}
+			System.out.println("Got |G| = "+all_email_adresses.size());
+			String[] nodes = new String[all_email_adresses.size()];
+			int counter = 0;
+			for(String s : all_email_adresses) {
+				nodes[counter++] = s;
+			}
+			Arrays.sort(nodes);
+			HashMap<String, Integer> node_to_id = new HashMap<String, Integer>(nodes.length);
+			for(int id=0;id<nodes.length;id++) {
+				node_to_id.put(nodes[id], id);
+			}
+			
+			Graph g = new Graph(all_email_adresses.size());
+			for(String[] message : from) {
+				String mid = message[0];
+				final int start_vertex = node_to_id.get(message[1]);
+				ArrayList<String> recipients = to.get(mid);
+				if(recipients!=null) {
+					for(String node : recipients) {
+						int target_vertex = node_to_id.get(node);
+						//System.out.println(start_vertex+" "+target_vertex);
+						g.add_edge(start_vertex, target_vertex);
+					}
+				}else{
+					//System.err.println("recipients==null "+Arrays.toString(message));
+				}
+			}
+			System.out.println(g);
+			return g;
+		}else{
+			System.err.println(f+" does not exist");
+			return null;
+		}
+	}
+	
+	public static String advogato_edges = "./data/advogato_edges.csv";
+	public static String advogato_nodes = "./data/advogato_nodes.csv";
+	
+	static Graph get_advogato_graph() {
+		final int source_offset = 0;
+		final int target_offset = 1;
+		final int num_node = 6541;//According to advogato_nodes [0,6540]
+		Graph g = new Graph(num_node);
+		
+		File f = new File(advogato_edges);
+		if(f.exists()) {
+			try(BufferedReader br = new BufferedReader(new FileReader(f))){
+				String line;
+				br.readLine();//skip meta info
+				
+				while((line = br.readLine()) != null) {
+					String tokens[] = line.split(",");
+					int source = Integer.parseInt(tokens[source_offset]);
+					int target = Integer.parseInt(tokens[target_offset]);
+					if(source<num_node && source>=0) {
+						if(target<num_node && target>=0) {
+							g.add_edge(source, target);
+						}else{
+							System.err.println("target<num_node && target>=0");
+						}
+					}else{
+						System.err.println("source<num_node && source>=0");
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(g);
+			return g;
+		}else{
+			System.err.println(f+" does not exist");
+			return null;
+		}
+	}
+}
---
+++ src/graphs/Graph.java
@@ -0,0 +1,168 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Graph {
+	int id_e = 0;
+	
+	public int num_vertices;
+	private final ArrayList<Egde> edges = new ArrayList<Graph.Egde>();
+	
+	public Graph(int num_vertices) {
+		this.num_vertices = num_vertices;//They exist only virtually
+	}
+	
+	public Graph(boolean[][] adjacency_matrix) {
+		int num_lines 	= adjacency_matrix.length;
+		int num_columns = adjacency_matrix[0].length;
+		if(num_lines!=num_columns) {
+			System.err.println("Graph(bool[][]): num_lines!=num_columns");
+		}
+		this.num_vertices = num_lines;
+		for(int l=0;l<num_lines;l++) {
+			for(int c=0;c<num_columns;c++) {
+				if(adjacency_matrix[l][c]) {//there is an edge
+					new Egde(l, c);
+				}
+			}
+		}
+	}
+
+	public Graph(ArrayList<Integer>[] neighbor_list) {
+		this(neighbor_list.length);
+		for(int node=0;node<neighbor_list.length;node++) {
+			for(int target : neighbor_list[node]) {
+				new Egde(node,target);
+			}
+		}
+	}
+
+	class Egde{
+		final int id;
+		final int from;
+		final int to;
+		
+		Egde(int from, int to){
+			this.from = from;
+			this.to   = to;
+			this.id = get_next_edge_id();
+			edges.add(this);
+		}
+		
+		public String toString() {
+			return id+" ("+from+"->"+to+")";
+		}
+
+		public ArrayList<Integer> to_list() {
+			ArrayList<Integer> e = new ArrayList<Integer>(2);
+			e.add(from);
+			e.add(to);
+			return e;
+		}
+	}
+
+	final int get_next_edge_id() {
+		return id_e++;
+	}
+	
+	public int[][] get_edges_for_sanitation(){
+		int[][] e = new int[edges.size()][2];
+		for(int i=0;i<edges.size();i++) {
+			Egde my_edge = edges.get(i);
+			if(my_edge.from>this.num_vertices) {
+				System.err.println("get_edges_for_sanitation");
+			}
+			if(my_edge.to>this.num_vertices) {
+				System.err.println("get_edges_for_sanitation");
+			}
+			e[i][0] = my_edge.from;
+			e[i][1] = my_edge.to;
+		}
+		return e;
+	}
+	public ArrayList<Integer>[] get_neighbors(){
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer>[] edge_list = new ArrayList[this.num_vertices];
+		for(int i=0;i<this.num_vertices;i++) {
+			edge_list[i] = new ArrayList<Integer>();
+		}
+		for(Egde e : edges) {
+			if(edge_list[e.from].contains(e.to)) {
+				System.err.println("get_neighbors(): Duplicate edge");
+			}
+			edge_list[e.from].add(e.to);
+		}
+		return edge_list;
+	}
+	
+	public boolean[][] get_adjancency_matrix_as_bit_vector(){
+		boolean[][] neighbors = new boolean[this.num_vertices][this.num_vertices];
+		for(Egde e : edges) {
+			if(neighbors[e.from][e.to] == true) {
+				System.err.println("get_adjancency_matrix_as_bit_vector(): Duplicate edge");
+			}
+			neighbors[e.from][e.to] = true;
+		}
+		return neighbors;
+	}
+	
+	public static Graph get_example() {
+		Graph g = new Graph(60);
+		g.add_edge(1-1,2-1);
+		g.add_edge(2-1,4-1);
+		g.add_edge(3-1,1-1);
+		g.add_edge(3-1,2-1);
+		g.add_edge(4-1,3-1);
+		return g;
+	}
+
+	void add_edge(int from, int to) {
+		new Egde(from, to);
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Graph with "+this.num_vertices+" vertices and "+this.edges.size()+" edges");
+		
+		
+		//String ret = "Graph with "+this.num_vertices+" vertices and "+this.edges.size()+" edges";
+		int counter = 0;
+		for(Egde e : this.edges) {
+			if(counter > 10) break;
+			sb.append("\n"+e);
+			counter++;
+			//ret+="\n"+e;
+		}
+		return sb.toString();
+	}
+	
+	static void stop(double start, String text) {
+		System.out.println(text+" [DONE] in "+(System.currentTimeMillis()-start));
+	}
+	
+	public static Graph dedup_edges(Graph input) {
+		double start = System.currentTimeMillis();
+		System.out.println("dedup_edges(Graph g) Input:");
+		System.out.println(input);
+		
+		Graph output = new Graph(input.num_vertices);//creates graph with not edges
+		HashSet<ArrayList<Integer>> unique_edges = new HashSet<ArrayList<Integer>>(input.num_vertices);
+		int counter = 0;
+		for(Egde e : input.edges) {
+			unique_edges.add(e.to_list());
+			if(counter++%100000==0) {
+				System.out.println(counter);
+			}
+		}
+		
+		for(ArrayList<Integer> e_as_list : unique_edges) {
+			output.add_edge(e_as_list.get(0), e_as_list.get(1));
+		}
+		//Collections.sort(output.edges);
+		
+		stop(start, "dedup_edges(Graph g) Ouput:");
+		System.out.println(output);
+		return output;
+	}
+}
